95bd2252a90eb5e0299f5029f4173dbcafc34959
minh la <satthutit@gmail.com>
2018-05-08 12:02:05 +0900

    validate in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, :presence => true #validate khong duoc de rong
+  validates :price, :numericality => {:greater_than_or_equal_to => 1.0} #price phai la so va lon hon 1
+  validates :title, :uniqueness => true # khong cho phep title trung nhau
 end
