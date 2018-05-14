08f65cf34a75ca35bf4217a4afdb718f87b54d8f
minh la <satthutit@gmail.com>
2018-05-14 18:39:47 +0900

    Handle login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if user = User.authenticate(params[:name], params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, :alert => "Invalid username/password"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/', :notice => "Logged out"
   end
 end
