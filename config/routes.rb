83cee8aaec3a95e0bb35e9def75f024f1c448f44
minh la <satthutit@gmail.com>
2018-05-14 18:52:01 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,11 @@ Rails.application.routes.draw do
 
   root :to => 'store#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 end
