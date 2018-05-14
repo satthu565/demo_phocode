e503fb9ab3cb2d9eefd7c8d5880951c77362b8c5
minh la <satthutit@gmail.com>
2018-05-14 15:13:05 +0900

    Check current cart is empty?

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,12 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
+    @cart = current_cart
+    if @cart.line_items.empty?
+      redirect_to '/', :notice => 'Your cart is empty'
+      return
+    end
+
     @order = Order.new
   end
 
