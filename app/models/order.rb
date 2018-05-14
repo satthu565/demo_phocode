3667c2024d25be5964acd9ff2b7d71ee5e12495a
minh la <satthutit@gmail.com>
2018-05-14 15:21:51 +0900

    define relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  has_many :line_items, :dependent => :destroy
   PAYMENT_TYPES = [ "Cash on delivery", "Ngân lượng", "Bảo Kim", "Bank Card" ]
 
   validates :name, :address, :email, :pay_type, :presence => true
