d31422703371d109b19dbf12f4323520b6488937
minh la <satthutit@gmail.com>
2018-05-14 15:20:01 +0900

    validate order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   PAYMENT_TYPES = [ "Cash on delivery", "Ngân lượng", "Bảo Kim", "Bank Card" ]
+
+  validates :name, :address, :email, :pay_type, :presence => true
+  validates :pay_type, :inclusion => PAYMENT_TYPES
 end
