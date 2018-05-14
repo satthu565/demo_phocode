57f78d989c4779f9d4f65f98566c838017f79784
minh la <satthutit@gmail.com>
2018-05-14 15:32:14 +0900

    Create order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,9 +31,13 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(current_cart)
 
     respond_to do |format|
       if @order.save
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
