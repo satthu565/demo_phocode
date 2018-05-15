5b2b093abf454475ef6eb50daaf06993d4c99f77
minh la <satthutit@gmail.com>
2018-05-15 10:25:22 +0900

    Step 1

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,15 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
+
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, :on => :member
+    end
+    root :to => 'store#index', :as => 'store'
+  end
 end
